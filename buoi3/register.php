bb809acacde9ed2dfe4e37fc765696c73c77a50c
Chad Luong <apple.luong1905@gmail.com>
2022-03-10 14:55:15 +0700

    push 1

--- buoi3/register.php
+++ buoi3/register.php
@@ -7,39 +7,50 @@
 	<h1>Register form</h1>
 	<?php
 		$errUsername = $errPhone = $errEmail = $errGender = "";
-	?>
-	<form action="#" method="POST">
-		<p>Username*: 
-			<input type="text" name="username">
-		</p>
-		<p>Email*: 
-			<input type="text" name="email">
-		</p>
-		<p>Phone*: 
-			<input type="text" name="phone">
-		</p>
-		<p>Gender*: 
-			<input type="radio" name="gender" value="male"> Male
-			<input type="radio" name="gender" value="female"> Female
-		</p>
-		<p>
-			<input type="submit" name="register" value="Register">
-		</p>
-	</form>
-	<?php
+		$userName = $email = $phone = "";
 		if (isset($_POST['username'])) {
 			$userName = $_POST['username'];
 			$email = $_POST['email'];
 			$phone = $_POST['phone'];
 			$gender = "";
+
 			if (isset($_POST['gender'])) {
 				$gender = $_POST['gender'];
+			} else {
+				$errGender = "Please choose your gender";
 			}
-			echo $userName." <br>";
-			echo $email." <br>";
-			echo $phone." <br>";
-			echo $gender." <br>";
+			if ($_POST['username'] == '') {
+				$errUsername = "Please input your username";
+			}
+			if ($_POST['email'] == '') {
+				$errEmail = "Please input your email";
+			}
+			if ($_POST['phone'] == '') {
+				$errPhone = "Please input your phone";
+			}
 		}
 	?>
+	<form action="#" method="POST">
+		<p>Username*: 
+			<input type="text" name="username" value="<?php echo $userName?>">
+			<?php echo $errUsername;?>
+		</p>
+		<p>Email*: 
+			<input type="text" name="email" value="<?php echo $email?>">
+			<?php echo $errEmail;?>
+		</p>
+		<p>Phone*: 
+			<input type="text" name="phone" value="<?php echo $phone?>">
+			<?php echo $errPhone;?>
+		</p>
+		<p>Gender*: 
+			<input type="radio" name="gender" value="male" <?php echo ($gender == 'male')?'checked':'';?>> Male
+			<input type="radio" name="gender" value="female" <?php echo ($gender == 'female')?'checked':'';?>> Female
+			<?php echo $errGender;?>
+		</p>
+		<p>
+			<input type="submit" name="register" value="Register">
+		</p>
+	</form>
 </body>
 </html>
